f374a7d0b82564b6e40e9b68674be9cdfa451708
Khalid Saifullah <khalideofficial@gmail.com>
2022-02-11 00:47:12 +0600

    Site is done

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ const faqs = document.querySelectorAll('.faq_wrapper')
 faqs.forEach((faq) => {
     faq.addEventListener('click', () => {
         faqs.forEach((faq) => {
-            faq.classList.remove('faq_active')
+            faq.classList.add('faq_active')
         })
         faq.classList.toggle('faq_active')
     })
@@ -18,4 +18,20 @@ ben_cards.forEach((item) => {
         })
         item.classList.add('demo_details_card_active')
     })
+})
+
+// Toggle Mobile Menu
+const menuBtn = document.querySelector('.mobile_menu_icon')
+const nav = document.querySelector('.nav')
+
+menuBtn.addEventListener('click', () => {
+    nav.classList.toggle('nav_active')
+    
+    if(menuBtn.classList.contains('icofont-navigation-menu')) {
+        menuBtn.classList.remove('icofont-navigation-menu')
+        menuBtn.classList.add('icofont-close')
+    }else{
+        menuBtn.classList.add('icofont-navigation-menu')
+        menuBtn.classList.remove('icofont-close')
+    }
 })
